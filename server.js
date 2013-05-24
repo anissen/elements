140aba3f5f1176f5704f62ff172bc864f46eac79
Anders Nissen <ani@marimatech.com>
2013-05-24 16:04:28 +0200

    Added example socket.io usage for testing

--- server.js
+++ server.js
@@ -24,7 +24,22 @@ fs.readdirSync(models_path).forEach(function (file) {
 // bootstrap passport config
 require('./config/passport')(passport, config);
 
-var app = express();
+var app = express(),
+    http = require('http'),
+    server = http.createServer(app),
+    io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { text: 'hello world' });
+  socket.on('ping', function (data) {
+    socket.emit('pong', 'in response to ' + data)
+  })
+});
+
+// TODO: Move this to a configuration file (e.g. express.js)
+io.configure('development', function(){
+  io.set('log level', 2);
+});
 
 // express settings
 require('./config/express')(app, config, passport);
@@ -34,5 +49,5 @@ require('./config/routes')(app, passport, auth);
 
 // Start the app by listening on <port>
 var port = process.env.PORT || 3000;
-app.listen(port);
+server.listen(port);
 console.log('Express app started on port '+port);
